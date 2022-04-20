0930675a592a229e37d7649176b7dd21b7c915ff
Pedro Nogueira Barboza <pedro.nogueira.barboza@gmail.com>
2022-04-20 13:48:01 -0300

    adicionei os arquivos novos do projeto e coloquei as retas

--- Desafio_01/canvas.js
+++ Desafio_01/canvas.js
@@ -9,11 +9,11 @@ c.fillStyle = 'rgba (0, 255,0,0.5)';
 //c.fillRect(100,100,100,100); //serve para falar as dimensões
 
 //linha
-c.beginPath(); //inicio de comando 
-c.moveTo(50,500); //início da linha
-c.lineTo(50,200); // fim da linha
-c.strokeStyle = "green" // cor da linha
-c.stroke()// começa a desenhar
+// c.beginPath(); //inicio de comando 
+// c.moveTo(50,500); //início da linha
+// c.lineTo(50,200); // fim da linha
+// c.strokeStyle = "green" // cor da linha
+// c.stroke()// começa a desenhar
 
 //círculo 
 
@@ -45,6 +45,7 @@ c.stroke()// começa a desenhar
 // }
 
 for(var quadrados=0,posx=0,posy=10; quadrados<10;quadrados++){
+    c.fillStyle = 'red'
     var altura=100;
     var comprimento=100;
     //posy=10
@@ -57,14 +58,28 @@ for(var quadrados=0,posx=0,posy=10; quadrados<10;quadrados++){
 for(var circulos=0,posx=50,posy=200; circulos<10;circulos++){
    
    //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    c.strokeStyle = "#F22005"
+    c.fillStyle = 'blue'
     c.beginPath()//inicio do comando 
     c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
-    c.strokeStyle = "#F22005"
     c.stroke()
     posx= posx+110
 
     // posy = posy+110;
 }
+
+for(var retas=0,posx=50,posy=200; retas<10;retas++){
+   
+    //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    c.beginPath(); //inicio de comando 
+    c.moveTo(posx,500); //início da linha
+    c.lineTo(posx,200); // fim da linha
+    c.strokeStyle = "green" // cor da linha
+    c.stroke()// começa a desenhar
+     posx= posx+110
+ 
+     // posy = posy+110;
+ }
 
 // for(var circulos=0,posx=Math.random(),posy=Math.random(); circulos<10;circulos++){
    
---
+++ Desafio_02/canvas.js
@@ -0,0 +1,101 @@
+var canvas = document.querySelector('canvas'); //funciona com o nome do elemento 
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d'); // c é pincel do canva, ou seja o 'super object'
+
+c.fillStyle = 'rgba (0, 255,0,0.5)';
+//c.fillRect(100,100,100,100); //serve para falar as dimensões
+
+//linha
+// c.beginPath(); //inicio de comando 
+// c.moveTo(50,500); //início da linha
+// c.lineTo(50,200); // fim da linha
+// c.strokeStyle = "green" // cor da linha
+// c.stroke()// começa a desenhar
+
+//círculo 
+
+// c.beginPath()//inicio do comando 
+// c.arc(50,200,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+// c.strokeStyle = "#F22005"
+// c.stroke()
+
+
+
+// let quadrados = 0;
+// let retas = 0;
+// let circulos = 0;
+
+//let posx =0;
+// let posy=0;
+
+// Math.floor(Math.random() * 21) - 10;
+
+// for(quadrados;quadrados<=10;quadrados++){
+    
+//     var altura=100;
+//     var comprimento=100;
+//     c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    
+//     posx= posx+110;
+//     posy = posy+110;
+
+// }
+
+for(var quadrados=0,posx=0,posy=10; quadrados<10;quadrados++){
+    c.fillStyle = 'red'
+    var altura=100;
+    var comprimento=100;
+    //posy=10
+    c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    posx= posx+110;
+  
+    // posy = posy+110;
+}
+
+for(var circulos=0,posx=50,posy=200; circulos<10;circulos++){
+   
+   //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    c.strokeStyle = "#F22005"
+    c.fillStyle = 'blue'
+    c.beginPath()//inicio do comando 
+    c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+    c.stroke()
+    posx= posx+110
+
+    // posy = posy+110;
+}
+
+for(var retas=0,posx=50,posy=200; retas<10;retas++){
+   
+    //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+    c.beginPath(); //inicio de comando 
+    c.moveTo(posx,500); //início da linha
+    c.lineTo(posx,200); // fim da linha
+    c.strokeStyle = "green" // cor da linha
+    c.stroke()// começa a desenhar
+     posx= posx+110
+ 
+     // posy = posy+110;
+ }
+
+// for(var circulos=0,posx=Math.random(),posy=Math.random(); circulos<10;circulos++){
+   
+//     //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+//      c.beginPath()//inicio do comando 
+//      c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+//      c.strokeStyle = "#F22005"
+//      c.stroke()
+//      posx= posx+1000
+ 
+//      // posy = posy+110;
+//  }
+
+
+// while(circulos<10){
+ 
+//     c.arc(400,400,50,0, Math.PI*2,false)
+
+// }
