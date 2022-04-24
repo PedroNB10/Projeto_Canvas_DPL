f5db7a6b37342a6af3412a65810592bad877a2fd
= <=>
2022-04-24 07:49:56 -0300

    código do desafio 01 reorganizado

--- Desafio_01/canvas.js
+++ Desafio_01/canvas.js
@@ -1,90 +1,70 @@
+
 var canvas = document.querySelector('canvas'); //funciona com o nome do elemento 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d'); // c é pincel do canva, ou seja o 'super object'
 
-
-for(var quadrados=0,posx=50,posy=30; quadrados<10;quadrados++){
-
-
-     c.fillStyle = 'red'
-     var altura=100;
-    var comprimento=100;
-    
-   
-    c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
-    
-    posx= posx+110;
+for (var quadrados = 0, posx = 50, posy = 30; quadrados < 10; quadrados++) {
+    c.fillStyle = 'red'
+    var altura = 100;
+    var comprimento = 100;
+    c.fillRect(posx, posy, altura, comprimento); //serve para falar as dimensões
+    posx = posx + 110;
 
 }
 
-for(var circulos=0,posx=100,posy=200; circulos<30;circulos++){
-   
-   //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
-   if(circulos<10){
-    
+for (var circulos = 0, posx = 100, posy = 200; circulos < 30; circulos++) {
+    if (circulos < 10) {
 
-    
+        c.beginPath()//inicio do comando 
+        c.arc(posx, posy, 50, 0, Math.PI * 2, false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+        c.strokeStyle = "black"
+        c.stroke()
+        c.fillStyle = 'green'
+        c.fill()
+        posx = posx + 110
+    }
 
-    c.beginPath()//inicio do comando 
-    c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+    else if (circulos == 11) {
 
-     c.strokeStyle = "black"
-     c.stroke()
-    c.fillStyle = 'green'
-    c.fill()
-    posx= posx+110
-   }
-
-    else if(circulos==11){
-       
         posx = 100
         posy = 325
-    
-            for(var i=0; i<10; i++){
-                
-                
-                c.beginPath()//inicio do comando 
-                c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
-                c.strokeStyle = "black"
-                c.stroke()
-                c.fillStyle = 'yellow'
-                c.fill()
-                posx = posx+110
-              }   
-        }
 
-        else if(circulos==21){
-       
-            posx = 100
-            posy = 450
-        
-                for(var i=0; i<10; i++){
-                    
-                    c.strokeStyle = "blue"
-                    c.beginPath()//inicio do comando 
-                    c.arc(posx,posy,50,0, Math.PI*2,false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
-                    // c.stroke()
-                    c.strokeStyle = "black"
-                    c.stroke()
-                    c.fillStyle = 'blue'
-                    c.fill()
-                    posx = posx+110
-                  }   
-            }
+        for (var i = 0; i < 10; i++) {
+            c.beginPath()//inicio do comando 
+            c.arc(posx, posy, 50, 0, Math.PI * 2, false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+            c.strokeStyle = "black"
+            c.stroke()
+            c.fillStyle = 'yellow'
+            c.fill()
+            posx = posx + 110
+        }
+    }
+
+    else if (circulos == 21) {
+        posx = 100
+        posy = 450
+        for (var i = 0; i < 10; i++) {
+            c.strokeStyle = "blue"
+            c.beginPath()//inicio do comando 
+            c.arc(posx, posy, 50, 0, Math.PI * 2, false)  //c.arc (x, y, tamanho, angulo_inicial, angulo_final, anti_horario)
+            // c.stroke()
+            c.strokeStyle = "black"
+            c.stroke()
+            c.fillStyle = 'blue'
+            c.fill()
+            posx = posx + 110
+        }
+    }
 
 }
 
-for(var retas=0,posx=100,posy=200; retas<10;retas++){
-   
-    //  c.fillRect(posx,posy,altura,comprimento); //serve para falar as dimensões
+for (var retas = 0, posx = 100, posy = 200; retas < 10; retas++) {
     c.beginPath(); //inicio de comando 
-    c.moveTo(posx,150); //início da linha
-    c.lineTo(posx,500); // fim da linha
+    c.moveTo(posx, 150); //início da linha
+    c.lineTo(posx, 500); // fim da linha
     c.strokeStyle = "orange" // cor da linha
     c.stroke()// começa a desenhar
-    posx= posx+110
- 
- 
- }
+    posx = posx + 110
+}
 
